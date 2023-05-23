3a46e2f2ba0eac08de49bab8b0fd3092accdac40
Wicus <wicusbuys@gmail.com>
2023-05-23 13:17:28 +0200

    Images and product load

---
+++ php/getBestSellers.php
@@ -0,0 +1,29 @@
+<?php
+
+//Connect to MySQL database
+
+$conn = mysqli_connect("localhost", "root", "", "contemporarycarve");
+
+
+//Query the table
+
+$sql = "SELECT * FROM products";
+$result = $conn->query($sql);
+
+
+$data = array();
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        // Add each row to the data array
+        $data[] = $row;
+    }
+}
+
+// Close the database connection
+$conn->close();
+
+// Return data as JSON
+header('Content-Type: application/json');
+echo json_encode($data);
+?>
